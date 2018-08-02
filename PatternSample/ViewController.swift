95c0d09f09e585651a691325e9ff34a931ffdc18
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-03 01:05:30 +0900

    TextViewの追加

--- PatternSample/ViewController.swift
+++ PatternSample/ViewController.swift
@@ -14,10 +14,15 @@ protocol ViewControllerDelegate: class {
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextView: UITextView!
     weak var transitionDelegate: ViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TextView.isUserInteractionEnabled = true
+        TextView.isEditable = true
+        TextView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,5 +42,17 @@ class ViewController: UIViewController {
         transitionDelegate?.pushProductPage(productId: 3)
     }
     
+}
+
+extension ViewController:UITextViewDelegate{
+   
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            //あなたのテキストフィールド
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
 }
 
